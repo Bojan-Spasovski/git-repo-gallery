c240373114fa502e766decf85a15d47903d8d794
Bojan_S <bojanspasovski1@gmail.com>
2021-05-28 17:48:27 +0200

    added twon new functions (fetch-repos) and (display repos)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // div where profile information will appear.
 const profileInfo = document.querySelector(".overview");
 const username = "Bojan-Spasovski";
+const repoList = document.querySelector(".repo-list");
 
 const gitProfile = async function () {
-    const gitProfile = await fetch(`https://api.github.com/users/${username}`);
-    const data = await gitProfile.json();
+  const gitProfile = await fetch(`https://api.github.com/users/${username}`);
+  const data = await gitProfile.json();
 
-    displayUserInfo(data);
+  displayUserInfo(data);
 };
 
 gitProfile();
@@ -14,9 +15,9 @@ gitProfile();
 
 
 const displayUserInfo = function (data) {
-    const div = document.createElement("div");
-    div.classList.add("user-info");
-    div.innerHTML = `<figure>
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `<figure>
     <img alt="user avatar" src=${data.avatar_url} />
   </figure>
   <div>
@@ -25,7 +26,26 @@ const displayUserInfo = function (data) {
     <p><strong>Location:</strong> ${data.location}</p>
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div> `;
-    profileInfo.append(div);
+  profileInfo.append(div);
+  fetchRepoList();
+};
+
+const fetchRepoList = async function () {
+  const repoLink = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await repoLink.json();
+  // console.log(repoData);
+  displayRepoInfo(repoData);
+};
+
+// fetchRepoList();
+
+const displayRepoInfo = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
 
 
